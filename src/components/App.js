aa3e13f3307900be6fa1b60d97d5d1e419caca7c
ZachMoore <Get3Moore@gmail.com>
2018-11-12 09:07:06 -0500

    Add kiln state

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import Kiln from '../lib/kiln';
+let kiln = new Kiln()
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      kiln: {}
+    }
+
+    this.updatePackage = () => {
+
+      kiln.getPackage()
+        .then(data=>data.json())
+        .then(object => {
+          this.setState({kiln: object})
+        })
+    }
+  }
+
+  componentDidMount(){
+    this.updatePackage()
+
+    setInterval(this.updatePackage, 1000)
+  }
+
   render() {
     return (
       <div className="App">
-        Smart Kiln
+        <h1>Temperature: {this.state.kiln.temperature}</h1>
       </div>
     );
   }
